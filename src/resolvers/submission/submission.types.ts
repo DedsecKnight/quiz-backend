e2d7aaf9cc14e91ed695811019c0087fca587fff
Mike Nguyen <mikenguyen7268@gmail.com>
2021-07-15 15:12:05 -0500

    Rewrite submit resolver to fetch ID of submission's author from token instead of user input

--- src/resolvers/submission/submission.types.ts
+++ src/resolvers/submission/submission.types.ts
@@ -3,7 +3,6 @@ import { SubmissionArg } from "../../interfaces/ISubmissionRepo";
 
 @InputType()
 export class SubmitInput implements SubmissionArg {
-    @Field(() => ID)
     userId: number;
 
     @Field(() => ID)
